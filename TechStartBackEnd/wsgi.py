b75d97d3e0ffddd86b658bc498e5deca9497e04b
Richard <51135299+rzwc@users.noreply.github.com>
2021-04-19 21:39:41 -0600

    change wsgi.py file

--- TechStartBackEnd/wsgi.py
+++ TechStartBackEnd/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechStartBackEnd.TechStartBackEnd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechStartBackEnd.settings')
 
 application = get_wsgi_application()
