cf54e627cfbbc8a6c0550b5715ed4c38bdf6a3de
Richard <51135299+rzwc@users.noreply.github.com>
2021-04-24 22:24:58 -0600

    add cors allowed origins

--- TechStartBackEnd/settings.py
+++ TechStartBackEnd/settings.py
@@ -159,3 +159,7 @@ django_heroku.settings(locals())
 # prod_db  =  dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:9000"
+]
