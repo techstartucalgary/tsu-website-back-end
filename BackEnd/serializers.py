c30ad03b3b4ca785d4026e06d3ee9193aa939bd8
Richard <51135299+rzwc@users.noreply.github.com>
2021-05-03 13:56:13 -0600

    fix brackets

--- BackEnd/serializers.py
+++ BackEnd/serializers.py
@@ -52,9 +52,9 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password':{'write_only': True},
         }
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username']),
+        user = User.objects.create_user(validated_data['username'],
         first_name = validated_data['first_name'],
         last_name = validated_data['last_name'],
         email = validated_data['email'],
-        password = validated_data['password']
+        password = validated_data['password'])
         return user
