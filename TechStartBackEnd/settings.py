b050c7da18c0b38cb12d73975882b60e920d43c1
Richard <51135299+rzwc@users.noreply.github.com>
2021-06-06 20:55:41 -0600

    add swagger documentation and first/last name fields to post

--- TechStartBackEnd/settings.py
+++ TechStartBackEnd/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'BackEnd',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- TechStartBackEnd/urls.py
+++ TechStartBackEnd/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from BackEnd import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 
 urlpatterns = [
@@ -39,5 +56,9 @@ urlpatterns = [
     path('comment/<int:pk>/delete', views.deleteComment.as_view(), name='Delete Comment'),
     path('login', views.customObtainAuthToken.as_view(), name='API Token Login'),
     path('register', views.RegisterUserView.as_view(), name='Register user'),
-    path('ping', views.pingAppView, name='Ping Heroku Server')
+    path('ping', views.pingAppView, name='Ping Heroku Server'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   
 ]
